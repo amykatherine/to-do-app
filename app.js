99971fc0f29d8b9391ed0e5363d179fee765f76b
Amy Duong <akduong91@gmail.com>
2019-02-04 12:44:55 -0800

    assignment 6 completed

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
 function onReady() {
-  const toDos = [];
+  let id = 0;
+  let toDos = []; //using let because we are going to add and remove items from the array
   const addToDoForm = document.getElementById('addToDoForm');
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
+    // if newToDoText doesn't have a value, get out of the function
     if (!newToDoText.value) { return; }
 
+    // create an object
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
+
+    id++;
     newToDoText.value = '';
 
     renderTheUI();
   }
-
+  // renderTheUI function is where we create all UI's related things
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
+    // allow to say when adding new todo, the li is not repeating itself
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
+      newLi.textContent = toDo.title;
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = "Delete";
 
-      newLi.textContent = toDo.title;
+      deleteButton.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+        });
+
+        renderTheUI();
+      });
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
     });
   }
 
@@ -37,9 +53,7 @@ function onReady() {
     createNewToDo();
   });
 
-  renderTheUI(); {
-
-  }
+  renderTheUI();
 }
 
 window.onload = function() {
